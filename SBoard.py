27bf090488cfe4f06f6581364fa7c9d1e83ffe4f
lasl7800 <33358174+lasl7800@users.noreply.github.com>
2020-01-29 18:59:59 -0500

    Update SBoard.py

--- SBoard.py
+++ SBoard.py
@@ -1,10 +1,30 @@
+'''
+Created on Jan. 28, 2020
+
+@author: nicolelaslavic
+'''
 # Import socket module
 from socket import * 
 import sys # In order to terminate the program
 
+
+boardWidth = sys.argv[2]    # Width of board
+boardHeight = sys.argv[3]  # Height of board
+print("board width: ", boardWidth)
+print("board height: ", boardHeight)
+colour_list = []
+board = []
+
+for i in range(4, 8):
+    print("colour: ", sys.argv[i])
+    colour_var = sys.argv[i].upper()
+    colour_list.append(colour_var)
+  
+    
 # Create a TCP server socket
 #(AF_INET is used for IPv4 protocols)
 #(SOCK_STREAM is used for TCP)
+
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
@@ -20,90 +40,92 @@ serverSocket.listen(1)
 print ('The server is ready to receive')
 
 # Server should be up and running and listening to the incoming connections
-class Note(object):
+
+
+class note(object):
     message= ""
     colour = ""
     x = 0
     y=0
     height=0
-    pins=0
     width=0
 
-    # The class "constructor" - It's actually an initializer ,pin
-    def __init__(self, x, y, width,height, colour,message,pins):
+    # The class "constructor" - It's actually an initializer 
+    def init(self, x, y, width,height,colour,message):
         self.message = message
         self.colour = colour
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        self.pins=pins
-        
-Board=[]
+
 while True:
     #print('The server is ready to receive')
 
     # Set up a new connection from the client
     connectionSocket, addr = serverSocket.accept()
 
-    
-    sentence = connectionSocket.recv(1024).decode()
-    
+
+    sentence = connectionSocket.recv(2048).decode()
+
     sent=sentence.split()
-    
+
     request= sent[0].upper()
     
-    if request=='PIN':
-        print("PIN")
-        result = 'PIN'
         
+    if request=='PIN':
+        
+        result = 'PIN'
+    
         #do something
     elif request=='GET':
-        print("GET")
         
+    
         result = "GET"
-        #do something
+            #do something
     elif request == 'UNPIN':
         print("UNPIN")
         result = "UNPIN"
-        #do something
-        
+            #do something
+    
     elif request =='POST':
-        print("POST")
-        cb =' '
-        new_message= cb.join(sent[6:])
-        
-        note= Note(sent[1], sent[2],sent[3], sent[4], sent[5],new_message,0)
-        Board.append(note)
-
-        result = 'POST'
-        
+        note.x = sent[1]
+        note.y = sent[2]
+        note.width = sent[3]
+        note.height = sent[4]
+        note.colour = sent[5]
+        note.message = sent[6].upper()
+        if ((note.x + note.width) <= boardWidth) and ((note.y + note.height) <= boardHeight):
+            if (colour_list.contains(note.colour)):
+                result = 'POST'
+                board.append(note.message)
+            else:
+                result = "Choose colour that is available for note"
+        else:
+            result = 'note not within the board parameters'
+    elif request == 'CLEAR':
+        result = "CLEAR"
+    
     elif request=='CONNECT':
+    
         
-        print("connecting")
         result = "connecting"
-        #do something
+            #do something
     elif request=='DISCONNECT':
         
-        print("DISCONNECT")
         result = "disconnecting"
-        #do something
-    elif request== 'CLEAR':
-        
-        for i in Board:
-            Board.pop(i)
-            
-        result='Board now clear'
-        
+        serverSocket.close()
+            #do something
     else:
         result="nothing"
-        
-    connectionSocket.sendall(result.encode())
-
     
-    connectionSocket.close()
-
-
-
-serverSocket.close()  
+      
+        #capitalizedSentence = sentence.upper()
+  
+        
+        
+        
+        connectionSocket.close()
+        connectionSocket.sendall(result.encode())
+        
 sys.exit()#Terminate the program after sending the corresponding data
